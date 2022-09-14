ae48630e964484fce996956aa631e1b757c18cd2
edgegum <104591550+edgegum@users.noreply.github.com>
2022-09-13 23:12:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 
 const { MongoClient } = require("mongodb");
 
-//const uri = "mongodb+srv://edgegum_sa_east:mK8VfFVI9IrOMbl3@edge-sa-east.e3ftd.mongodb.net/sample_mflix?retryWrites=true&w=majority"
-const uri = "mongodb+srv://edgegum_sa_east:pCduin@m0ng0@edge-sa-east.e3ftd.mongodb.net/?retryWrites=true&w=majority/sample_restaurants?retryWrites=true&w=majority"
+const uri = "mongodb+srv://edgegum_sa_east:mK8VfFVI9IrOMbl3@edge-sa-east.e3ftd.mongodb.net/sample_mflix?retryWrites=true&w=majority"
+//const uri = "mongodb+srv://edgegum_sa_east:pCduin@m0ng0@edge-sa-east.e3ftd.mongodb.net/?retryWrites=true&w=majority/sample_restaurants?retryWrites=true&w=majority"
 // use the express-static middleware
 app.use(express.static("public"));
 
@@ -16,14 +16,14 @@ app.get("/api/movie", async function (req, res) {
   try {
     await client.connect();
 
-    //const database = client.db('sample_mflix');
-    //const collection = database.collection('movies');
-    const database = client.db('sample_restaurants');
-    const collection = database.collection('restaurants');
+    const database = client.db('sample_mflix');
+    const collection = database.collection('movies');
+    //const database = client.db('sample_restaurants');
+    //const collection = database.collection('restaurants');
 
     // Query for a movie that has the title 'Back to the Future'
-    const query = { cuisine: "American", poster: { $exists: true } };
-    //const query = { genres: "Comedy", poster: { $exists: true } };
+    //const query = { cuisine: "American", poster: { $exists: true } };
+    const query = { genres: "Comedy", poster: { $exists: true } };
     const cursor = await collection.aggregate([
       { $match: query },
       { $sample: { size: 1 } },
